c1c868aff7f5235996b43c785fe0b1a5cb944d8b
Marcin Figiel <gibki5555@wp.pl>
2020-06-03 20:41:46 +0200

    Modul 2 Refactory

---
+++ kodilla-intro/src/main/java/marcin/Application.java
@@ -0,0 +1,8 @@
+package marcin;
+
+public class Application {
+    public static void main(String[] args) {
+        Person person = new Person(40.5, 178, "Adam");
+        person.checkAgeAndHeight();
+    }
+}
---
+++ kodilla-intro/src/main/java/marcin/Person.java
@@ -0,0 +1,24 @@
+package marcin;
+
+public class Person {
+    String name;
+    double age;
+    double height;
+
+    public Person(double age, double height, String name) {
+        this.age = age;
+        this.height = height;
+        this.name = name;
+
+    }
+
+    public void checkAgeAndHeight() {
+        if (this.name != null) {
+            if (this.age > 30 && this.height > 160) {
+                System.out.println("User is older than 30 and higher then 160cm");
+            } else {
+                System.out.println("User is younger than 30 or lower than 160cm");
+            }
+        }
+    }
+}
