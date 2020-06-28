99ba4af415c640f732dd17e94587152d05360fae
Marcin Figiel <gibki5555@wp.pl>
2020-06-28 21:04:36 +0200

    temp

--- kodilla-intro/src/main/java/marcin/CalcAplication.java
+++ kodilla-intro/src/main/java/marcin/CalcAplication.java
@@ -3,7 +3,7 @@ package marcin;
 public class CalcAplication {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        double area = calculator.circleArea(r:10);
+        double area = calculator.circleArea(10);
         System.out.println("Area of the Circle with radius 10 equals: " + area);
 
         double p = Calculator.PI;
